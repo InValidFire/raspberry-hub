bb4ada1dd58d464b0c65d08191a3d2580e212ee6
InValidFire <wolvie2013@gmail.com>
2020-11-25 05:14:54 -0500

    fix spotify

--- modules/spotify.py
+++ modules/spotify.py
@@ -31,7 +31,6 @@ def set_token(code, request: Request):
     return RedirectResponse(f"{get_site()}/spotify")
 
 @router.get("/spotify")
-@token_required
 def spotify(request: Request, token = None):
     access_token = get_token()
     api = spotipy.Spotify(access_token)
